00ff180927a9bf0fcec5ff169a1401d852b7bf36
kystyn <konstantindamaskinskiy@gmail.com>
2019-11-01 19:54:42 +0300

    some comments added

--- editor/editor.h
+++ editor/editor.h
@@ -5,6 +5,7 @@
 
 #include "menu.h"
 
+/* Representation methods */
 typedef enum
 {
     VIEW = MENU_VIEW,
@@ -19,17 +20,25 @@ typedef struct tagTEXTDATA {
 } TEXTDATA;
 
 typedef struct tagTEXTRNDDATA {
-    int xLeftUp, yLeftUp;
-    int symsPerW, symsPerH;
-    int curLineInStr;
+    int xLeftUp, yLeftUp;   /* Current upper left (x, y) point */
+    int symsPerW, symsPerH; /* symbols per linear size */
+    int curLineInStr;       /* cur line in str (actual in layout
+                             * mode. in view - zero
+                             */
 } TEXTRNDDATA;
 
 BOOL readFile( char const *name, TEXTDATA *data );
 int findMaxStrWidth( TEXTDATA *td, int yStart, int yEnd );
 void freeTextData( TEXTDATA *td );
+
+/* number - number of string in document */
 int strByteLength( TEXTDATA *td, int number );
 int strTextLength( TEXTDATA *td, int number );
 int linesInCurStr( int strTL, TEXTRNDDATA *trd );
+
+/* find document end method.
+ * endYLeftUp, endCurLineInStr are TEXTRNDDATA parameters
+ * which will let us see document finish*/
 void endOfDocument( MODE m, TEXTDATA *td, TEXTRNDDATA *trd,
                     int *endYLeftUp, int *endCurLineInStr );
 
--- editor/utils.h
+++ editor/utils.h
@@ -3,17 +3,21 @@
 
 #include "editor.h"
 
+/* Move text with controls methods */
 void LineUp( MODE m, TEXTDATA *td, TEXTRNDDATA *trd );
 void LineDown( MODE m, TEXTDATA *td, TEXTRNDDATA *trd );
 void PageUp( MODE m, TEXTDATA *td, TEXTRNDDATA *trd );
 void PageDown( MODE m, TEXTDATA *td, TEXTRNDDATA *trd );
 
+/* Mapping from text representation to scroll space functions group */
 int textWidthToHScroll( TEXTDATA *td, TEXTRNDDATA *trd, int minScroll, int maxScroll );
 int textHeightToVScroll( TEXTDATA *td, TEXTRNDDATA *trd, int minScroll, int maxScroll );
 
+/* Inverse mapping */
 int hScrollToTextWidth( TEXTDATA *td, TEXTRNDDATA *trd, int scrollPos, int minScroll, int maxScroll );
 int vScrollToTextHeight( int endYLeftUp, int scrollPos, int minScroll, int maxScroll );
 
+/* Invalidate screen method */
 void invalidateScreen( HWND hWnd, TEXTRNDDATA *trd, TEXTMETRIC *tm );
 
 #endif // __UTILS_H_
